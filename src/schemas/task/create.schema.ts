3fd166a7cda6d1b8d6185835a38bd45941265441
Gabriel <foioamaral16@gmail.com>
2022-09-26 17:34:15 -0300

    feat: working schemas

---
+++ src/schemas/task/create.schema.ts
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+const createTaskSchema = yup.object().shape({
+  description: yup.string().required(),
+});
+
+const responseObject = {
+  id: yup.string().uuid().required(),
+  description: yup.string().required(),
+  is_complete: yup.boolean().required(),
+};
+
+const newShape = Object.entries(responseObject)
+  .reverse()
+  .reduce((prev, [key, value]) => ({ ...prev, [key]: value }), {});
+
+const serializedCreateTaskSchema = yup.object().shape(newShape);
+
+export { createTaskSchema, serializedCreateTaskSchema };
